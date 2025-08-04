af079d419e0e55b4ffef88b4bada8bd02bbb20de
kaity6 <202271975+Kaity6@users.noreply.github.com>
2025-08-04 09:25:11 +0000

    first

---
+++ script.js
@@ -0,0 +1,8 @@
+let buttons = document.getElementById("buttons")
+
+for (let i = 0; i < 10; i++) {
+    let madeButton = document.createElement("button")
+    madeButton.innerText = i
+    madeButton.classList.add("button")
+    buttons.appendChild(madeButton)
+}
