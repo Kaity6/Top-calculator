269dd3eb4f7ab515a3db21355e4f9355a4f7f809
kaity6 <202271975+Kaity6@users.noreply.github.com>
2025-08-15 16:01:06 +0000

    struggling

--- script.js
+++ script.js
@@ -7,13 +7,16 @@ for (let i = 0; i < 10; i++) {
     madeButton.setAttribute("id", `button${i}`)
     buttons.appendChild(madeButton)
 }
-let result = 6
-let b;
+let result = 6;
+let storeCondition = false;
 let screen = document.getElementById("screen")
 screen.innerText = result
 
+
 buttons.addEventListener("click", () => {
     screen.innerText = result;
+    storeCondition = false;
+    trigger()
 })
 
 
@@ -22,7 +25,6 @@ AC.addEventListener("click", function () {
     result = 0
 })
 
-let c = false
 
 let button0 = document.getElementById("button0")
 let button1 = document.getElementById("button1")
@@ -37,55 +39,62 @@ let button9 = document.getElementById("button9")
 
 button1.addEventListener("click", function () {
     result = 1
-    a ? b = 1 : b = null
-    console.log(b)
 })
 button2.addEventListener("click", function () {
-    result = 2
-    a ? b = 2 : b = null
+   
+   result = 2 
+   
 })
 button3.addEventListener("click", function () {
-    result = 3
-    a ? b = 3 : b = null
+   
+   result = 3
 })
 button4.addEventListener("click", function () {
     result = 4
-    a ? b = 4 : b = null
 })
 button5.addEventListener("click", function () {
     result = 5
-    a ? b = 5 : b = null
 })
 button6.addEventListener("click", function () {
-    result = 6
-    a ? b = 6 : b = null
+  result =  6
 })
 button7.addEventListener("click", function () {
-    result = 7
-    a ? b = 7 : b = null
+   result = 7
 })
 button8.addEventListener("click", function () {
-    result = 8
-    a ? b = 8 : b = null
+   result = 8
 })
 button9.addEventListener("click", function () {
     result = 9
-    a ? b = 9 : b = null
 })
 button0.addEventListener("click", function () {
-    result = 0
-    a ? b = 0 : b = null
+   result = 0
 })
 
-function calc() {
-    if (b) {
-        result = result + b
-    }
+
+function plusOperation() {
+    storeCondition = true;
+    return result
 }
 
-let plus = document.getElementById("plus")
-plus.addEventListener("click", () => {
-    let a = true
-    calc()
-})
 
+plus = document.getElementById("plus");
+plus.addEventListener("click", plusOperation)
+
+
+function trigger () {
+if (storeCondition) {
+    let a = plusOpearation();
+    if (storeCondition = false) {
+        let b = result;
+        result = a + b
+    }
+}
+}
+
+
+let equal = document.getElementById("equal")
+function call () {
+    console.log(result)
+}
+equal.addEventListener("click", call)
