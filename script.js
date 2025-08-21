c7d560998d2a476acb8c9669af7e25e20407b662
kaity6 <202271975+Kaity6@users.noreply.github.com>
2025-08-21 19:13:09 +0000

    finally figured it out

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let buttons = document.getElementById("buttons")
-
+let numbers = document.getElementById("numbers")
 for (let i = 0; i < 10; i++) {
     let madeButton = document.createElement("button")
     madeButton.innerText = i
     madeButton.classList.add("button")
     madeButton.setAttribute("id", `button${i}`)
-    buttons.appendChild(madeButton)
+    numbers.appendChild(madeButton)
 }
 
 let result = 6;
@@ -23,6 +23,7 @@ screen.innerText = result
 let AC = document.getElementById("AC")
 AC.addEventListener("click", function () {
     result = 0
+    screen.innerText = result
 })
 
 
@@ -75,28 +76,61 @@ let previousResult;
 let operator;
 let newResult;
 let plus = document.getElementById("plus")
+let minus = document.getElementById("minus")
+let multiply = document.getElementById("multiply")
+let divide = document.getElementById("divide")
+
 
 plus.addEventListener("click", () => {
-    operation = "+"
     storeCondition = true;
-    console.log("meow")
-})
-
-buttons.addEventListener("click", () => {
     previousResult = result;
-    screen.innerText = result;
+    operator = "+"
 })
 
-if (storeCondition) {
-    buttons.addEventListener("click", () => {
+minus.addEventListener("click", () => {
+    storeCondition = true;
+    previousResult = result;
+    operator = "-"
+})
+
+
+multiply.addEventListener("click", () => {
+    storeCondition = true;
+    previousResult = result;
+    operator = "*"
+})
+
+divide.addEventListener("click", () => {
+    storeCondition = true;
+    previousResult = result;
+    operator = "/"
+})
+
+
+
+numbers.addEventListener("click", () => {if (storeCondition) {
+
         newResult = result
-        result = `${previousResult} ${operator} ${newResult}`
-        console.log(result)
+        
+            if (operator === "+") {
+                result = previousResult + newResult
+            } else if (operator === "-") {
+                result = previousResult - newResult
+            } else if (operator === "*") {
+                result = previousResult * newResult
+            } else {
+                result = previousResult / newResult
+            }
+
         storeCondition = false
-    })
+        screen.innerText = result
 } else {
     
     previousResult = result;
     screen.innerText = result;
     
 }
+})
+
+console.log(storeCondition)
+
